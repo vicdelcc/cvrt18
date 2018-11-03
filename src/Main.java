6c444edc7e08342e419e7f8fda2e5cc6929106fc
vicdev89 <victor.del.campo@student.htw-berlin.de>
2018-11-03 19:28:40 +0100

    .

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,63 @@
+import java.util.*;
+
 public class Main {
 
+    private static final String BEARBEITUNGSZEIT = "Bearbeitungszeit";
+    private static final String SOLLENDTERMIN = "Soll-Endtermin";
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        System.out.println("########### Reihenfolgeproblem mit NEH-Heuristik ###########");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Wie viele Aufträge?");
+        int anzahlAuftraege = scanner.nextInt();
+
+        List<Character> auftraegeNamen = new ArrayList<>();
+        for (int i = 65; i < 65 + anzahlAuftraege; i++) {
+            auftraegeNamen.add((char) i);
+        }
+
+        HashMap<Character, HashMap<String, Integer>> daten = new HashMap<>();
+
+        for (int i = 1; i <= anzahlAuftraege; i++) {
+            System.out.println("Auftrag Nr. " + i + ":");
+            System.out.println("Bearbeitungszeit: ");
+            int bearbeitungszeit = scanner.nextInt();
+            System.out.println("Soll-Endtermin: : ");
+            int sollEndetermin = scanner.nextInt();
+
+            HashMap<String, Integer> zeiten = new HashMap<>();
+            zeiten.put(BEARBEITUNGSZEIT, bearbeitungszeit);
+            zeiten.put(SOLLENDTERMIN, sollEndetermin);
+
+            daten.put(auftraegeNamen.get(i - 1), zeiten);
+        }
+
+
+        // Beispiel Permutation 2 erste Elemente
+
+        for(int i = 0; i< getFakultät(2); i++) {
+
+        }
+        HashMap<String, Integer> auftragA = daten.get('A');
+        HashMap<String, Integer> auftragB = daten.get('B');
+
+
+        int verpaetungA = auftragA.get(BEARBEITUNGSZEIT) - auftragA.get(SOLLENDTERMIN) < 0 ? 0 : auftragA.get(BEARBEITUNGSZEIT) - auftragA.get(SOLLENDTERMIN);
+        int verspaetungB = auftragA.get(BEARBEITUNGSZEIT) + auftragB.get(BEARBEITUNGSZEIT) - auftragB.get(SOLLENDTERMIN) < 0 ? 0 : auftragA.get(BEARBEITUNGSZEIT) + auftragB.get(BEARBEITUNGSZEIT) - auftragB.get(SOLLENDTERMIN);
+
+        System.out.println("Verspätung Auftrag A: " + verpaetungA);
+        System.out.println("Verspätung Auftrag B: " + verspaetungB);
+
+
+    }
+
+    public static int getFakultät(int zahl) {
+        int fakultaet = 1;
+        for (int i=1; i<=zahl; i++) {
+            fakultaet = fakultaet * i;
+        }
+        return fakultaet;
     }
 }
